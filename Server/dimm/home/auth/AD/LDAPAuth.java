9905c1c1a471e9221cfc20e94717eba61d92a67f
Mark Williams <mark@dimm.de>
2009-09-16 18:28:32 +0000

    LDAP

--- Server/dimm/home/auth/AD/LDAPAuth.java
+++ Server/dimm/home/auth/AD/LDAPAuth.java
@@ -4,10 +4,7 @@
  */
 package dimm.home.auth.AD;
 
-import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.NamingEnumeration;
@@ -15,175 +12,15 @@ import javax.naming.NamingException;
 import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
 import javax.naming.directory.BasicAttributes;
-import javax.naming.directory.DirContext;
-import javax.naming.directory.InitialDirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 import javax.naming.ldap.InitialLdapContext;
 import javax.naming.ldap.LdapContext;
 
-class Test2
-{
 
-    DirContext context;
-
-    void init() throws NamingException
-    {
-        Hashtable<String, String> env = new Hashtable<String, String>();
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(Context.SECURITY_PRINCIPAL, "Administrator");
-        env.put(Context.SECURITY_CREDENTIALS, "helikon");
-        env.put(Context.PROVIDER_URL, "ldap://my.server.address");
-
-        env.put("java.naming.ldap.attributes.binary", "objectSid");
-        env.put(Context.REFERRAL, "follow");
-
-        context = new InitialDirContext(env);
-
-    }
-
-    void search() throws NamingException
-    {
-        SearchControls ctrl = new SearchControls();
-        ctrl.setSearchScope(SearchControls.SUBTREE_SCOPE);
-        ctrl.setReturningAttributes(new String[]
-                {
-                    "objectSid"
-                });
-
-        Attributes attributes = context.getAttributes(context.getNameInNamespace());
-        Attribute attribute = attributes.get("defaultNamingContext");
-        String rootSearchBase = attribute.get().toString();
-
-        NamingEnumeration<SearchResult> enumeration =
-                context.search(rootSearchBase, "(objectclass=user)", ctrl);
-
-        // And to invoke the method using a SID from one of the entries fetched earlier
-        SearchResult o = enumeration.next(); //This is the enumeration object obtained on step II above
-        Attributes res_attr = o.getAttributes();
-        String SID = getSIDAsString((byte[]) res_attr.get("objectsid").get());
-
-    }
-
-    public static String getSIDAsString( byte[] SID )
-    {
-        // Add the 'S' prefix
-        StringBuilder strSID = new StringBuilder("S-");
-
-        // bytes[0] : in the array is the version (must be 1 but might
-        // change in the future)
-        strSID.append(SID[0]).append('-');
-
-        // bytes[2..7] : the Authority
-        StringBuilder tmpBuff = new StringBuilder();
-        for (int t = 2; t <= 7; t++)
-        {
-            String hexString = Integer.toHexString(SID[t] & 0xFF);
-            tmpBuff.append(hexString);
-        }
-        strSID.append(Long.parseLong(tmpBuff.toString(), 16));
-
-        // bytes[1] : the sub authorities count
-        int count = SID[1];
-
-        // bytes[8..end] : the sub authorities (these are Integers - notice
-        // the endian)
-        for (int i = 0; i < count; i++)
-        {
-            int currSubAuthOffset = i * 4;
-            tmpBuff.setLength(0);
-            tmpBuff.append(String.format("%02X%02X%02X%02X",
-                    (SID[11 + currSubAuthOffset] & 0xFF),
-                    (SID[10 + currSubAuthOffset] & 0xFF),
-                    (SID[9 + currSubAuthOffset] & 0xFF),
-                    (SID[8 + currSubAuthOffset] & 0xFF)));
-
-            strSID.append('-').append(Long.parseLong(tmpBuff.toString(), 16));
-        }
-
-        // That's it - we have the SID
-        return strSID.toString();
-    }
-}
-
-class JndiAction
-{
-    //Der Vollqualifizierte Name des Administrators im AD
-    // final static String ADMIN_NAME = "CN=Administrator,CN=Users,DC=home,DC=dimm";
-
-    final static String ADMIN_NAME = "Administrator";
-    final static String ADMIN_PASSWORD = "helikon";
-    //User Standardpfad von dem die Suche im AD ausgehen soll
-    static LdapContext ctx;
-
-    public Integer run() throws Exception
-    {
-
-        init();
-        List list = findUsersByAccountName("");
-        for (Iterator iter = list.iterator(); iter.hasNext();)
-        {
-            String element = (String) iter.next();
-            System.out.println(element);
-        }
-        ctx.close();
-        return 0;
-    }
-
-    static void init() throws Exception
-    {
-
-        Hashtable env = new Hashtable();
-        env.put(Context.INITIAL_CONTEXT_FACTORY,
-                "com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(Context.SECURITY_AUTHENTICATION, "simple");
-//        env.put(Context.SECURITY_AUTHENTICATION, "GSSAPI");
-        env.put(Context.SECURITY_PRINCIPAL, ADMIN_NAME);
-        env.put(Context.SECURITY_CREDENTIALS, ADMIN_PASSWORD);
-        //Der entsprechende Domänen-Controller:LDAP-Port
-        env.put(Context.PROVIDER_URL, "ldap://192.168.1.120:389");
-        ctx = new InitialLdapContext(env, null);
-    }
-
-    static List findUsersByAccountName( String accountName ) throws Exception
-    {
-        List list = new ArrayList();
-
-
-        //Unsere LDAP Abfrage...
-        String searchFilter = "(&(objectCategory=person)(objectClass=user)(name=M*))";
-
-
-        //System.out.println(searchFilter);
-
-        //Wir definiren den "Suchapparat" für die LDAP Suche...
-        SearchControls searchControls = new SearchControls();
-        String[] resultAttributes =
-        {
-            "sn", "givenName", "sAMAccountName"
-        };
-        searchControls.setReturningAttributes(resultAttributes);
-        searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
-
-        //Wir führen die Suche im LDAP durch
-        String search_base = "CN=Users,DC=dimm,DC=home";
-
-        NamingEnumeration results = ctx.search(search_base, searchFilter, searchControls);
-
-        //Wir iterieren über alle Resultate und speichern die gefundenen Namen
-        //in einer Liste.
-        while (results.hasMoreElements())
-        {
-            SearchResult searchResult = (SearchResult) results.nextElement();
-            list.add(searchResult.toString());
-        }
-        return list;
-    }
-}
 
 class UserContext
 {
-
     String dn;
     LdapContext ctx;
 
@@ -194,7 +31,7 @@ class UserContext
     }
 }
 
-public class LoginTest
+public class LDAPAuth
 {
 
     String admin_name;
@@ -205,7 +42,7 @@ public class LoginTest
     LdapContext ctx;
     String error_txt;
 
-    public LoginTest( String admin_name, String admin_pwd, String ldap_host, int ldap_port, boolean ssl )
+    public LDAPAuth( String admin_name, String admin_pwd, String ldap_host, int ldap_port, boolean ssl )
     {
         this.admin_name = admin_name;
         this.admin_pwd = admin_pwd;
@@ -219,12 +56,12 @@ public class LoginTest
         System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
     }
 
-    boolean is_connected()
+    public boolean is_connected()
     {
         return (ctx != null);
     }
 
-    boolean connect_ldap()
+    public boolean connect()
     {
         try
         {
@@ -343,14 +180,6 @@ public class LoginTest
 
     String get_user_attribute( UserContext uctx, String attr_name )
     {
-/*        SearchControls ctrl = new SearchControls();
-        ctrl.setSearchScope(SearchControls.OBJECT_SCOPE);
-        ctrl.setReturningAttributes(new String[]
-                {
-                    attr_name
-                });
-*/
-
         Attributes search_attributes = new BasicAttributes("distinguishedName", uctx.dn);
 
         String[] return_attributes = new String[1];
@@ -393,8 +222,8 @@ public class LoginTest
     {
         try
         {
-            LoginTest test = new LoginTest("Administrator", "helikon", "192.168.1.120", 0, /*ssl*/false);
-            if (test.connect_ldap())
+            LDAPAuth test = new LDAPAuth("Administrator", "helikon", "192.168.1.120", 0, /*ssl*/false);
+            if (test.connect())
             {
                 UserContext uctx = test.open_user( "mark@localhost", "12345" );
                 if (uctx != null)
@@ -404,27 +233,11 @@ public class LoginTest
                     test.close_user(uctx);
                 }
             }
-
-
         }
         catch (Exception exc)
         {
             exc.printStackTrace();
         }
     }
-    /**
-     * Wir initialisieren die Verbindung zum LDAP Service
-     * Die zum Zugriff notwendigen Daten legen wir in einer
-     * Hashtable ab und übergeben diese an den InitialLdapContext, der
-     * mit den darin enthaltenen Informationen die Verbindung aufbaut.
-     * @throws Exception
-     */
-    /**
-     * Wir bekommen ein Namenskürzel hineingereicht und bauen uns damit einen
-     * LDAP Suchfilter um nach dem vermeindlichen Benutzer zu suchen. Die gefundenen
-     * Kandidaten werden in einer Liste zurückgegeben.
-     * @param accountName
-     * @return
-     * @throws Exception
-     */
+  
 }
