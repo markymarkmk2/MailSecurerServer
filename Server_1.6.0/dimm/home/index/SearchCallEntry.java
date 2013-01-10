0afd0d87a1cef9e4177d297cf0045faa016e496a
Mark Williams <mark@dimm.de>
2013-01-10 10:27:20 +0000

    Close expired search Allow Range Search

--- Server_1.6.0/dimm/home/index/SearchCallEntry.java
+++ Server_1.6.0/dimm/home/index/SearchCallEntry.java
@@ -14,18 +14,25 @@ import home.shared.SQL.UserSSOEntry;
 public class SearchCallEntry
 {
 
-    SearchCall call;
-    int id;
-    UserSSOEntry ssoc;
+    public static final long MAX_AGE_MS = 86400*1000l;
+    private SearchCall call;
+    private int id;
+    private UserSSOEntry ssoc;
+    private long created;
 
     public SearchCallEntry( UserSSOEntry ssoc, SearchCall call, int id )
     {
         this.call = call;
         this.id = id;
         this.ssoc = ssoc;
+        created = System.currentTimeMillis();
+    }
+    int getId()
+    {
+        return id;
     }
 
-    String get_id()
+    String get_SceId()
     {
         return "sc" + id;
     }
@@ -34,9 +41,14 @@ public class SearchCallEntry
     {
         return ssoc;
     }
-    public SearchCall get_call()
+    public SearchCall getCall()
     {
         return call;
+    }
+    public boolean isExpired()
+    {
+        long age = System.currentTimeMillis() - created;
+        return  (age > MAX_AGE_MS);
     }
     
 }
